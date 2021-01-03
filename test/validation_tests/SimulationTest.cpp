9b2d2ca8c8053dcfb4cb1a0379a018471d84ca25
Piotr Kubala <pkua.log@gmail.com>
2021-01-03 19:59:46 +0100

    Validation tests for multiple interaction centres

--- test/validation_tests/SimulationTest.cpp
+++ test/validation_tests/SimulationTest.cpp
@@ -9,10 +9,12 @@
 #include "core/LatticeArrangingModel.h"
 #include "core/shapes/SphereTraits.h"
 #include "core/shapes/SpherocylinderTraits.h"
+#include "core/shapes/KMerTraits.h"
 #include "core/PeriodicBoundaryConditions.h"
 #include "core/interactions/LennardJonesInteraction.h"
+#include "core/interactions/RepulsiveLennardJonesInteraction.h"
 
-TEST_CASE("Simulation: equilibration for dilute hard sphere gas") {
+TEST_CASE("Simulation: equilibration for dilute hard sphere gas", "[short]") {
     // We choose temperature 10 and pressure 1. For particles of radius 0.05 we should obtain number density 0.0999791
     // We start with density 0.01 and too small step ranges. The program should adjust and equilibrate correctly
     auto pbc = std::make_unique<PeriodicBoundaryConditions>();
@@ -35,7 +37,7 @@ TEST_CASE("Simulation: equilibration for dilute hard sphere gas") {
     CHECK(density.error / density.value < 0.03); // up to 3%
 }
 
-TEST_CASE("Simulation: degenerate hard sphere gas") {
+TEST_CASE("Simulation: degenerate hard sphere gas", "[short]") {
     // We choose temperature 10 and pressure 1. For particles of radius 0.05 we should obtain number density 0.0999791
     // We start with density 0.01 and too small step ranges. The program should adjust and equilibrate correctly
     auto pbc = std::make_unique<PeriodicBoundaryConditions>();
@@ -58,7 +60,7 @@ TEST_CASE("Simulation: degenerate hard sphere gas") {
     CHECK(density.error / density.value < 0.03); // up to 3%
 }
 
-TEST_CASE("Simulation: slightly degenerate hard spherocylinder gas") {
+TEST_CASE("Simulation: slightly degenerate hard spherocylinder gas", "[short]") {
     // We choose temperature 10 and pressure 1. For particles of radius 0.05 we should obtain number density 0.0999791
     // We start with density 0.01 and too small step ranges. The program should adjust and equilibrate correctly
 
@@ -82,7 +84,7 @@ TEST_CASE("Simulation: slightly degenerate hard spherocylinder gas") {
     CHECK(density.error / density.value < 0.03); // up to 3%
 }
 
-TEST_CASE("Simulation: slightly degenerate Lennard-Jones gas") {
+TEST_CASE("Simulation: slightly degenerate Lennard-Jones gas", "[short]") {
     // For parameters chosen compressibility factor should be around 1.2 and equation of state seem to be well
     // approximated by the second virial coefficient known analytically
     auto pbc = std::make_unique<PeriodicBoundaryConditions>();
@@ -104,4 +106,53 @@ TEST_CASE("Simulation: slightly degenerate Lennard-Jones gas") {
     INFO("Monte Carlo density: " << density);
     CHECK(density.value == Approx(expected).margin(density.error * 3)); // 3 sigma tolerance
     CHECK(density.error / density.value < 0.03); // up to 3%
+}
+
+TEST_CASE("Simulation: hard dumbbell fluid", "[short]") {
+    // Semi-Theoretical values from "An equation of state for hard dumbell fluids"
+    // D.J. Tildesley a & W.B. Streett (1980)
+    auto pbc = std::make_unique<PeriodicBoundaryConditions>();
+    double V = 500;
+    double linearSize = std::cbrt(V);
+    auto shapes = LatticeArrangingModel{}.arrange(50, std::cbrt(V));
+    KMerTraits kmerTraits(2, 0.5, 1);
+    auto packing = std::make_unique<Packing>(linearSize, std::move(shapes), std::move(pbc), kmerTraits.getInteraction());
+    // More frequent averaging here to preserve short simulation times (particle displacement are large anyway)
+    Simulation simulation(1, 2, 10, 1, 10, 5000, 5000, 100, 1234);
+    std::ostringstream loggerStream;
+    Logger logger(loggerStream);
+
+    simulation.perform(std::move(packing), kmerTraits.getInteraction(), logger);
+
+    Quantity density = simulation.getAverageDensity();
+    double expected = 0.3043317608769238;
+    INFO("Tildesley-Streett density: " << expected);
+    INFO("Monte Carlo density: " << density);
+    CHECK(density.value == Approx(expected).margin(density.error * 3)); // 3 sigma tolerance
+    CHECK(density.error / density.value < 0.01); // up to 1%
+}
+
+TEST_CASE("Simulation: wca dumbbell fluid", "[medium]") {
+    // Value for density remorselessly stolen from
+    // https://github.com/glotzerlab/hoomd-blue/blob/master/hoomd/hpmc/validation/wca_dumbbell.py
+    auto pbc = std::make_unique<PeriodicBoundaryConditions>();
+    double V = 500;
+    double linearSize = std::cbrt(V);
+    auto shapes = LatticeArrangingModel{}.arrange(50, std::cbrt(V));
+    auto interaction = std::make_unique<RepulsiveLennardJonesInteraction>(1, 1);
+    KMerTraits kmerTraits(2, 0.5, 1, std::move(interaction));
+    auto packing = std::make_unique<Packing>(linearSize, std::move(shapes), std::move(pbc), kmerTraits.getInteraction());
+    // More frequent averaging here to preserve short simulation times (particle displacement are large anyway)
+    Simulation simulation(1, 7.5, 10, 1, 10, 5000, 5000, 100, 1234);
+    std::ostringstream loggerStream;
+    Logger logger(loggerStream);
+
+    simulation.perform(std::move(packing), kmerTraits.getInteraction(), logger);
+
+    Quantity density = simulation.getAverageDensity();
+    double expected = 0.43451;
+    INFO("hoomd-blue density: " << expected);
+    INFO("Monte Carlo density: " << density);
+    CHECK(density.value == Approx(expected).margin(density.error * 3)); // 3 sigma tolerance
+    CHECK(density.error / density.value < 0.01); // up to 1%
 }
