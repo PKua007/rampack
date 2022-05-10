c0dcdc9048e284a9091360aff8c2dad9827abbca
Piotr Kubala <pkua.log@gmail.com>
2022-05-10 19:35:40 +0200

    Additional BondOrder test

--- test/unit_tests/core/BondOrderTest.cpp
+++ test/unit_tests/core/BondOrderTest.cpp
@@ -14,7 +14,7 @@
 
 #include <iostream>
 
-TEST_CASE("BondOrder") {
+TEST_CASE("BondOrder: hexatic") {
     // We are creating a packing with 2 layers. Each layer has (n1 x n2) unit cells and is a perfect honeycomb which
     // has been squashed 0.8 times in one direction. The 2 layer are stacked on one another distant by shift[2] and also
     // shifted in XY direction by {shift[0], shift[1]} to form a triclinic box
@@ -81,4 +81,25 @@ TEST_CASE("BondOrder") {
     // Values calculated in mathematica
     CHECK(bondOrder.getIntervalValues()[0] == Approx(0.8014636892475136));
     CHECK(bondOrder.getIntervalValues()[1] == Approx(0.8800669379487272));
+}
+
+TEST_CASE("BondOrder: distance within layer") {
+    // We create two tetragonal layers shifted against each other. If the distance is calculated correctly - as a
+    // projection onto a layer - order should be perfect.
+    std::vector<Shape> shapes{Shape({1, 1, 0.25}), Shape({1, 3, 0.25}), Shape({1, 5, 0.25}),
+                              Shape({3, 1, 0.25}), Shape({3, 3, 0.25}), Shape({3, 5, 0.25}),
+                              Shape({5, 1, 0.25}), Shape({5, 3, 0.25}), Shape({5, 5, 0.25}),
+
+                              Shape({1.1, 1.1, 0.75}), Shape({1.1, 3.1, 0.75}), Shape({1.1, 5.1, 0.75}),
+                              Shape({3.1, 1.1, 0.75}), Shape({3.1, 3.1, 0.75}), Shape({3.1, 5.1, 0.75}),
+                              Shape({5.1, 1.1, 0.75}), Shape({5.1, 3.1, 0.75}), Shape({5.1, 5.1, 0.75})};
+    SphereTraits sphereTraits(0.1);
+    auto pbc = std::make_unique<PeriodicBoundaryConditions>();
+    TriclinicBox box(std::array<double, 3>{6, 6, 1});
+    Packing packing(box, shapes, std::move(pbc), sphereTraits.getInteraction(), 1, 1);
+    BondOrder bondOrder(4, {0, 0, 2});
+
+    bondOrder.calculate(packing, 1, 1, sphereTraits);
+
+    CHECK(bondOrder.getIntervalValues()[0] == Approx(1));
 }
