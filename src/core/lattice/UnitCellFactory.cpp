36b4916844d577b33fcd546bbfa03409b97f4a34
Piotr Kubala <pkua.log@gmail.com>
2022-05-18 20:50:50 +0200

    UnitCellFactory class

--- src/core/lattice/UnitCellFactory.cpp
+++ src/core/lattice/UnitCellFactory.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "UnitCellFactory.h"
+
+UnitCell UnitCellFactory::createScCell(const std::array<double, 3> &linearSize) {
+    return UnitCell(TriclinicBox(linearSize), {Shape({0.5, 0.5, 0.5})});
+}
+
+UnitCell UnitCellFactory::createBccCell(const std::array<double, 3> &linearSize) {
+    return UnitCell(TriclinicBox(linearSize), {Shape({0.25, 0.25, 0.25}), Shape({0.75, 0.75, 0.75})});
+}
+
+UnitCell UnitCellFactory::createFccCell(const std::array<double, 3> &linearSize) {
+    return UnitCell(TriclinicBox(linearSize), {Shape({0.25, 0.25, 0.25}),
+                                               Shape({0.75, 0.25, 0.25}),
+                                               Shape({0.25, 0.75, 0.25}),
+                                               Shape({0.25, 0.25, 0.75})});
+}
+
+UnitCell UnitCellFactory::createHcpCell(const std::array<double, 3> &cuboidalCellSize) {
+    std::vector<Shape> shapes{Shape({0, 0, 0}),
+                              Shape({0.5, 0.5, 0}),
+                              Shape({0, 1./3, 0.5}),
+                              Shape({0.5, 5./6, 0.5})};
+    // Center shapes in the cell
+    for (auto &shape : shapes)
+        shape.setPosition(shape.getPosition() + Vector<3>{0.25, 1./12, 0.25});
+
+    return UnitCell(TriclinicBox(cuboidalCellSize), std::move(shapes));
+}
+
+UnitCell UnitCellFactory::createHexagonalCell(const std::array<double, 3> &cuboidalCellSize) {
+    return UnitCell(TriclinicBox(cuboidalCellSize), {Shape({0.25, 0.25, 0.5}), Shape({0.75, 0.75, 0.5})});
+}
+
+UnitCell UnitCellFactory::createScCell(double linearSize) {
+    return UnitCellFactory::createScCell({linearSize, linearSize, linearSize});
+}
+
+UnitCell UnitCellFactory::createBccCell(double linearSize) {
+    return UnitCellFactory::createBccCell({linearSize, linearSize, linearSize});
+}
+
+UnitCell UnitCellFactory::createFccCell(double linearSize) {
+    return UnitCellFactory::createFccCell({linearSize, linearSize, linearSize});
+}
+
+UnitCell UnitCellFactory::createHcpCell(double ballDiameter) {
+    return UnitCellFactory::createHcpCell({ballDiameter, std::sqrt(3)*ballDiameter, 2*std::sqrt(6)*ballDiameter/3});
+}
+
+UnitCell UnitCellFactory::createHexagonalCell(double ballDiameter) {
+    return UnitCellFactory::createHexagonalCell({ballDiameter, std::sqrt(3)*ballDiameter, ballDiameter});
+}
--- src/core/lattice/UnitCellFactory.h
+++ src/core/lattice/UnitCellFactory.h
@@ -10,16 +10,20 @@
 
 class UnitCellFactory {
 public:
-    static UnitCell createScCell(double linearSize);
     static UnitCell createScCell(const std::array<double, 3> &linearSize);
-    static UnitCell createBccCell(double linearSize);
+    static UnitCell createScCell(double linearSize);
+
     static UnitCell createBccCell(const std::array<double, 3> &linearSize);
-    static UnitCell createFccCell(double linearSize);
+    static UnitCell createBccCell(double linearSize);
+
     static UnitCell createFccCell(const std::array<double, 3> &linearSize);
-    static UnitCell createHcpCell(double ballDiameter);
+    static UnitCell createFccCell(double linearSize);
+
     static UnitCell createHcpCell(const std::array<double, 3> &cuboidalCellSize);
-    static UnitCell createHexagonalCell(double ballDiameter);
+    static UnitCell createHcpCell(double ballDiameter);
+
     static UnitCell createHexagonalCell(const std::array<double, 3> &cuboidalCellSize);
+    static UnitCell createHexagonalCell(double ballDiameter);
 };
 
 
