2b1a543fbab273a8c9e2c23900780c531dcc4499
misiekc <misiekc@interia.pl>
2022-08-09 16:42:34 +0200

    RoundedConeTraits class

--- src/core/shapes/RoundedCone.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by ciesla on 8/9/22.
-//
-
-#include "RoundedCone.h"
---
+++ src/core/shapes/RoundedConeTraits.cpp
@@ -0,0 +1,62 @@
+//
+// Created by ciesla on 8/9/22.
+//
+
+#include "RoundedConeTraits.h"
+#include "geometry/xenocollide/BodyBuilder.h"
+
+
+MapPtr<CollideGeometry> RoundedConeTraits::createShapeModel(double r, double l) {
+    double R = 1;
+
+    BodyBuilder bb;
+    bb.sphere(R);
+    bb.move(0, 0, -l/2);
+    bb.sphere(r);
+    bb.move(0, 0, l/2);
+    bb.wrap();
+    return bb.getCollideGeometry();
+}
+
+double RoundedConeTraits::getVolume(double R, double r, double l) {
+    return
+            (M_PI*
+             (l*l*l*l*(r*r + r*R + R*R) +
+              2*l*l*(r - R)*(r - R)*(r*r + r*R + R*R) -
+              (r - R)*(r - R)*(r - R)*(r - R)*(r*r + r*R + R*R) +
+              2*l*l*l*(r*r*r + R*R*R))
+            )/(3*l*l*l);
+}
+
+RoundedConeTraits::RoundedConeTraits(double r, double l)
+        : XenoCollideTraits({0, 0, 1}, {1, 0, 0}, {0, 0, 0},
+                            RoundedConeTraits::getVolume(1, r, l),
+                            RoundedConeTraits::createShapeModel(r, l),
+                            l + 2*std::max(1., r)),
+          R{1.0}, r{r}, l{l}
+{
+
+}
+
+std::string RoundedConeTraits::toWolfram(const Shape &shape) const {
+    Vector<3> pos = shape.getPosition();
+    Matrix<3, 3> orientation = shape.getOrientation();
+
+    int ballsNo = 10;
+    std::stringstream out;
+    out << std::fixed;
+    out << "GeometricTransformation[" << std::endl;
+    out << "    {";
+    for(int i=0; i<ballsNo; i++){
+        out << "Sphere[{0, 0, "<< -l/2 + l*(((double)i)/(ballsNo-1)) << "}, " << ((ballsNo-1.0-i)*R + i*r)/(ballsNo-1) <<"]";
+        if (i<ballsNo-1)
+            out <<", ";
+    }
+    out << "}, " << std::endl;
+    out << "    AffineTransform[" << std::endl;
+    out << "        {{{" << orientation(0, 0) << ", " << orientation(0, 1) << ", " << orientation(0, 2) << "}," << std::endl;
+    out << "          {" << orientation(1, 0) << ", " << orientation(1, 1) << ", " << orientation(1, 2) << "}," << std::endl;
+    out << "          {" << orientation(2, 0) << ", " << orientation(2, 1) << ", " << orientation(2, 2) << "}}," << std::endl;
+    out << "          " << pos << "}]]";
+    return out.str();
+}
