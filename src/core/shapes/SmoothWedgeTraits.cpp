87f89315cc537d23acf7dee9f1beec9668aea595
Piotr Kubala <pkua.log@gmail.com>
2022-10-02 13:46:00 +0200

    SmoothWedgeTraits test + volume fix + range fix

--- src/core/shapes/SmoothWedgeTraits.cpp
+++ src/core/shapes/SmoothWedgeTraits.cpp
@@ -22,19 +22,21 @@ std::shared_ptr<AbstractXCGeometry> SmoothWedgeTraits::createShapeModel(double R
 }
 
 double SmoothWedgeTraits::getVolume(double R, double r, double l) {
-    return
-            (M_PI*
-             (l*l*l*l*(r*r + r*R + R*R) +
-              2*l*l*(r - R)*(r - R)*(r*r + r*R + R*R) -
-              (r - R)*(r - R)*(r - R)*(r - R)*(r*r + r*R + R*R) +
-              2*l*l*l*(r*r*r + R*R*R))
-            )/(3*l*l*l);
+    double r2 = r*r;
+    double r3 = r2*r;
+    double R2 = R*R;
+    double R3 = R2*R;
+    double Rr = R*r;
+    double epsilon = (R - r)/l;
+    double epsilon2 = epsilon*epsilon;
+
+    return M_PI*l/3*(R2 + Rr + r2)*(1 + epsilon2) + 2*M_PI/3*(R3 + r3);
 }
 
 SmoothWedgeTraits::SmoothWedgeTraits(double R, double r, double l)
-        : XenoCollideTraits({0, 0, 1}, {1, 0, 0}, {0, 0, 0},
+        : XenoCollideTraits({0, 0, 1}, {1, 0, 0}, {0, 0, (r - R)/2},
                             SmoothWedgeTraits::getVolume(R, r, l),
-                            l + 2*std::max(1., r),
+                            l + 2*std::max(R, r),
                             {{"sl", {0, 0, -l/2}}, {"ss", {0, 0, l/2}}}),
           R{R}, r{r}, l{l}, shapeModel{SmoothWedgeTraits::createShapeModel(R, r, l)}
 {
---
+++ test/unit_tests/core/shapes/SmoothWedgeTraitsTest.cpp
@@ -0,0 +1,44 @@
+//
+// Created by pkua on 02.10.22.
+//
+
+#include <catch2/catch.hpp>
+
+#include "matchers/VectorApproxMatcher.h"
+
+#include "core/shapes/SmoothWedgeTraits.h"
+
+
+TEST_CASE("SmoothWedge: geometry") {
+    SmoothWedgeTraits traits(2, 1, 5);
+    const auto &geometry = traits.getGeometry();
+    const auto &interaction = traits.getInteraction();
+
+    Shape shape({1, 2, 3}, Matrix<3, 3>::rotation(0, M_PI/2, 0));
+    CHECK_THAT(geometry.getPrimaryAxis(shape), IsApproxEqual({1, 0, 0}, 1e-12));
+    CHECK_THAT(geometry.getSecondaryAxis(shape), IsApproxEqual({0, 0, -1}, 1e-12));
+    CHECK_THAT(geometry.getGeometricOrigin(shape), IsApproxEqual({-0.5, 0, 0}, 1e-12));
+    // Calculated in Mathematica using the formula, cross-checked with numerical convex hull volume
+    CHECK(geometry.getVolume() == Approx(272*M_PI/15));
+    CHECK_THAT(geometry.getNamedPoint("cm", shape), IsApproxEqual({1, 2, 3}, 1e-12));
+    CHECK_THAT(geometry.getNamedPoint("o", shape), IsApproxEqual({0.5, 2, 3}, 1e-12));
+    CHECK_THAT(geometry.getNamedPoint("ss", shape), IsApproxEqual({3.5, 2, 3}, 1e-12));
+    CHECK_THAT(geometry.getNamedPoint("sl", shape), IsApproxEqual({-1.5, 2, 3}, 1e-12));
+    CHECK(interaction.getRangeRadius() == 9);
+}
+
+TEST_CASE("SmoothWedge: collide geometry") {
+    SmoothWedgeTraits traits(2, 1, 5);
+    const auto &collideGeometry = traits.getCollideGeometry();
+
+    CHECK_THAT(collideGeometry.GetCenter(), IsApproxEqual({0, 0, 0}, 1e-12));
+
+    // We are always using not normalized normal vectors, because they should work
+    CHECK_THAT(collideGeometry.GetSupportPoint({0, 0, 2}), IsApproxEqual({0, 0, 3.5}, 1e-12));
+    CHECK_THAT(collideGeometry.GetSupportPoint({0, 0, -2}), IsApproxEqual({0, 0, -4.5}, 1e-12));
+    double b = 0.2;
+    double h1 = 0.4*std::sqrt(6);
+    auto sideNormal = Vector<3>{h1, 0, b};
+    auto tangentPoint = Vector<3>{h1, 0, 2.5 + b};
+    CHECK(sideNormal * (collideGeometry.GetSupportPoint(sideNormal) - tangentPoint) == Approx(0).margin(1e-12));
+}
