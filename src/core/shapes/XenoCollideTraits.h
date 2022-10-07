fd08f99e418c0c544e77f0e7dd23f483a61b16d3
Piotr Kubala <pkua.log@gmail.com>
2022-10-07 21:37:49 +0200

    XenoCollideTraits refactoring

--- src/core/shapes/XenoCollideTraits.h
+++ src/core/shapes/XenoCollideTraits.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <utility>
 #include <sstream>
+#include <optional>
 
 #include "core/ShapeTraits.h"
 #include "geometry/xenocollide/AbstractXCGeometry.h"
@@ -24,6 +25,9 @@ private:
     double volume{};
     std::map<std::string, Vector<3>> customNamedPoints{};
 
+    mutable std::optional<double> rangeRadius;
+    mutable std::optional<XCPrinter::Polyhedron> polyhedron;
+
 public:
     static constexpr std::size_t MESH_SUBDIVISIONS = 3;
     
@@ -31,9 +35,7 @@ public:
                       std::map<std::string, Vector<3>> customNamedPoints = {})
             : primaryAxis{pa}, secondaryAxis{sa}, geometricOrigin{o}, volume{v},
               customNamedPoints{std::move(customNamedPoints)}
-    {
-
-    }
+    { }
 
     [[nodiscard]] const Interaction &getInteraction() const override { return *this; }
     [[nodiscard]] const ShapeGeometry &getGeometry() const override { return *this; }
@@ -59,30 +61,35 @@ public:
 
     [[nodiscard]] bool overlapBetween(const Vector<3> &pos1, const Matrix<3, 3> &orientation1, std::size_t idx1,
                                       const Vector<3> &pos2, const Matrix<3, 3> &orientation2, std::size_t idx2,
-                                      const BoundaryConditions &bc) const override {
+                                      const BoundaryConditions &bc) const override
+    {
         const auto &thisConcreteTraits = static_cast<const ConcreteCollideTraits &>(*this);
         const auto &collideGeometry1 = thisConcreteTraits.getCollideGeometry(idx1);
         const auto &collideGeometry2 = thisConcreteTraits.getCollideGeometry(idx2);
         using XCGeometry = decltype(collideGeometry1);
-        double rangeRadius = collideGeometry1.getCircumsphereRadius() + collideGeometry2.getCircumsphereRadius();
+        double r = collideGeometry1.getCircumsphereRadius() + collideGeometry2.getCircumsphereRadius();
 
         Vector<3> pos2bc = pos2 + bc.getTranslation(pos1, pos2);
-        if ((pos2bc - pos1).norm2() > rangeRadius*rangeRadius)
+        if ((pos2bc - pos1).norm2() > r * r)
             return false;
-        bool result = XenoCollide<XCGeometry>::Intersect(collideGeometry1, orientation1, pos1, collideGeometry2, orientation2, pos2bc, 1.0e-12);
+        bool result = XenoCollide<XCGeometry>::Intersect(collideGeometry1, orientation1, pos1,
+                                                         collideGeometry2, orientation2, pos2bc,
+                                                         1.0e-12);
         return result;
     }
 
-    [[nodiscard]] bool overlapWithWall(const Vector<3> &pos, const Matrix<3, 3> &orientation, [[maybe_unused]] std::size_t idx,
-                                       const Vector<3> &wallOrigin, const Vector<3> &wallVector) const override {
-
+    [[nodiscard]] bool overlapWithWall(const Vector<3> &pos, const Matrix<3, 3> &orientation,
+                                       [[maybe_unused]] std::size_t idx, const Vector<3> &wallOrigin,
+                                       const Vector<3> &wallVector) const override
+    {
         const auto &thisConcreteTraits = static_cast<const ConcreteCollideTraits &>(*this);
         const auto &collideGeometry = thisConcreteTraits.getCollideGeometry(idx);
 
         Vector<3> normalVector = (orientation.transpose())*(wallVector);
-        Vector<3> sp = collideGeometry.getSupportPoint(-normalVector);
+        Vector<3> supportPoint = collideGeometry.getSupportPoint(-normalVector);
         Vector<3> origin = (orientation.transpose())*(wallOrigin - pos);
-        double distanceSupport = -sp*normalVector;  // minus sign because we count distance along -normalVector
+        // minus sign because we count distance along -normalVector
+        double distanceSupport = -supportPoint * normalVector;
         double distanceWall = -origin*normalVector;
         if (distanceWall > distanceSupport)
             return false;
@@ -90,6 +97,9 @@ public:
     }
 
     [[nodiscard]] double getRangeRadius() const override {
+        if (this->rangeRadius.has_value())
+            return *this->rangeRadius;
+
         std::size_t numCenters = std::max(this->getInteractionCentres().size(), 1ul);
         const auto &thisConcreteTraits = static_cast<const ConcreteCollideTraits &>(*this);
         double maxRadius{};
@@ -98,7 +108,8 @@ public:
             if (newRadius > maxRadius)
                 maxRadius = newRadius;
         }
-        return 2*maxRadius;
+        this->rangeRadius = 2*maxRadius;
+        return *this->rangeRadius;
     }
 
     [[nodiscard]] Vector<3> getNamedPoint(const std::string &pointName, const Shape &shape) const override {
@@ -119,20 +130,21 @@ public:
             centers.push_back({0, 0, 0});
 
         const auto &thisConcreteTraits = static_cast<const ConcreteCollideTraits &>(*this);
-        Matrix<3, 3> orientation = shape.getOrientation();
+        Matrix<3, 3> rot = shape.getOrientation();
         for (std::size_t i{}; i < centers.size(); i++) {
             const auto &geometry = thisConcreteTraits.getCollideGeometry(i);
             const auto &center = centers[i];
-            auto polyhedron = XCPrinter::buildPolyhedron(geometry, ConcreteCollideTraits::MESH_SUBDIVISIONS);
-            std::string wolframPolyhedron = polyhedron.toWolfram();
+            if (!this->polyhedron.has_value())
+                this->polyhedron.emplace(XCPrinter::buildPolyhedron(geometry, ConcreteCollideTraits::MESH_SUBDIVISIONS));
+            std::string wolframPolyhedron = this->polyhedron->toWolfram();
 
-            Vector<3> pos = shape.getPosition() + orientation * center;
+            Vector<3> pos = shape.getPosition() + rot * center;
 
             out << "GeometricTransformation[" << wolframPolyhedron << "," << std::endl;
             out << "AffineTransform[" << std::endl;
-            out << "    {{{" << orientation(0, 0) << ", " << orientation(0, 1) << ", " << orientation(0, 2) << "}," << std::endl;
-            out << "      {" << orientation(1, 0) << ", " << orientation(1, 1) << ", " << orientation(1, 2) << "}," << std::endl;
-            out << "      {" << orientation(2, 0) << ", " << orientation(2, 1) << ", " << orientation(2, 2) << "}}," << std::endl;
+            out << "    {{{" << rot(0, 0) << ", " << rot(0, 1) << ", " << rot(0, 2) << "}," << std::endl;
+            out << "      {" << rot(1, 0) << ", " << rot(1, 1) << ", " << rot(1, 2) << "}," << std::endl;
+            out << "      {" << rot(2, 0) << ", " << rot(2, 1) << ", " << rot(2, 2) << "}}," << std::endl;
             out << "      " << pos << "}]" << std::endl;
             out << "]";
 
