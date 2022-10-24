e4cdaa6c48db7e13cd035966a4dec029c7e3fecf
Piotr Kubala <pkua.log@gmail.com>
2022-10-24 21:57:53 +0200

    FourierTracker implementation

--- src/core/observables/trackers/FourierTracker.h
+++ src/core/observables/trackers/FourierTracker.h
@@ -5,13 +5,32 @@
 #ifndef RAMPACK_FOURIERTRACKER_H
 #define RAMPACK_FOURIERTRACKER_H
 
+#include <array>
+#include <functional>
+
 #include "core/observables/GoldestoneTracker.h"
 
 
 class FourierTracker : GoldestoneTracker {
 public:
-    [[nodiscard]] std::string getModeName() const override;
+    using Function = std::function<double(const Shape &, const ShapeTraits &)>;
 
+private:
+    using FourierFunction = std::function<double(double)>;
+    using FourierFunctions = std::array<std::vector<FourierFunction>, 3>;
+    using FourierValues = std::array<std::array<std::array<double, 2>, 2>, 2>;
+
+    std::array<std::size_t, 3> wavenumbers{};
+    Function function;
+    std::string functionName;
+    FourierFunctions fourierFunctions{};
+
+    [[nodiscard]] FourierValues calculateFourierValues(const Packing &packing, const ShapeTraits &shapeTraits) const;
+
+public:
+    FourierTracker(const std::array<std::size_t, 3> &wavenumber, Function function, std::string functionName);
+
+    [[nodiscard]] std::string getModeName() const override;
     void calculateOrigin(const Packing &packing, const ShapeTraits &shapeTraits) override;
 };
 
