9d296abb8c3ca38673678a813e264387b45c6212
Piotr Kubala <pkua.log@gmail.com>
2022-10-29 00:22:06 +0200

    FourierTracker refactoring

--- src/core/observables/trackers/FourierTracker.cpp
+++ src/core/observables/trackers/FourierTracker.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <algorithm>
-#include <iterator>
 
 #include "FourierTracker.h"
 #include "utils/Assertions.h"
@@ -14,32 +13,31 @@ std::string FourierTracker::getModeName() const {
     return this->functionName + "_fourier";
 }
 
-
 void FourierTracker::calculateOrigin(const Packing &packing, const ShapeTraits &shapeTraits) {
-    FourierValues fourierValues = this->calculateFourierValues(packing, shapeTraits);
-    Vector<3> originPosRel = this->calculateRelativeOriginPos(fourierValues);
+    FourierCoefficients fourierCoefficients = this->calculateFourierCoefficients(packing, shapeTraits);
+    Vector<3> originPosRel = this->calculateRelativeOriginPos(fourierCoefficients);
     originPosRel = this->normalizeOriginPos(originPosRel);
     this->originPos = packing.getBox().relativeToAbsolute(originPosRel);
 }
 
-Vector<3> FourierTracker::calculateRelativeOriginPos(const FourierTracker::FourierValues &fourierValues) const {
-    switch (this->nonzeroIdxs.size()) {
+Vector<3> FourierTracker::calculateRelativeOriginPos(const FourierTracker::FourierCoefficients &coefficients) const {
+    switch (this->nonzeroWavenumberIdxs.size()) {
         case 1:
-            return this->calculateRelativeOriginPos1D(fourierValues);
+            return this->calculateRelativeOriginPos1D(coefficients);
         case 2:
-            return this->calculateRelativeOriginPos2D(fourierValues);
+            return this->calculateRelativeOriginPos2D(coefficients);
         default:
-            throw AssertionException("FourierTracker: zeros = " + std::to_string(this->nonzeroIdxs.size()));
+            throw AssertionException("FourierTracker: zeros = " + std::to_string(this->nonzeroWavenumberIdxs.size()));
     }
 }
 
-Vector<3> FourierTracker::calculateRelativeOriginPos1D(const FourierTracker::FourierValues &fourierValues) const {
-    std::size_t nonzeroIdx = this->nonzeroIdxs.front();
+Vector<3> FourierTracker::calculateRelativeOriginPos1D(const FourierTracker::FourierCoefficients &coefficients) const {
+    std::size_t nonzeroIdx = this->nonzeroWavenumberIdxs.front();
     std::array<std::size_t, 3> idxs{};
-    idxs[nonzeroIdx] = 0;
-    double c = fourierValues[idxs[0]][idxs[1]][idxs[2]];
-    idxs[nonzeroIdx] = 1;
-    double d = fourierValues[idxs[0]][idxs[1]][idxs[2]];
+    idxs[nonzeroIdx] = COS;
+    double c = coefficients[idxs[0]][idxs[1]][idxs[2]];
+    idxs[nonzeroIdx] = SIN;
+    double d = coefficients[idxs[0]][idxs[1]][idxs[2]];
 
     if (c*c + d*d < AMPLITUDE_EPSILON*AMPLITUDE_EPSILON)
         return {};
@@ -50,28 +48,28 @@ Vector<3> FourierTracker::calculateRelativeOriginPos1D(const FourierTracker::Fou
     return relPos;
 }
 
-Vector<3> FourierTracker::calculateRelativeOriginPos2D(const FourierTracker::FourierValues &fourierValues) const {
-    std::size_t nonzeroIdx1 = this->nonzeroIdxs[0];
-    std::size_t nonzeroIdx2 = this->nonzeroIdxs[1];
+Vector<3> FourierTracker::calculateRelativeOriginPos2D(const FourierTracker::FourierCoefficients &fourierValues) const {
+    std::size_t nonzeroIdx1 = this->nonzeroWavenumberIdxs[0];
+    std::size_t nonzeroIdx2 = this->nonzeroWavenumberIdxs[1];
     std::array<std::size_t, 3> idxs{};
-    idxs[nonzeroIdx1] = 0;
-    idxs[nonzeroIdx2] = 0;
+    idxs[nonzeroIdx1] = COS;
+    idxs[nonzeroIdx2] = COS;
     double A = fourierValues[idxs[0]][idxs[1]][idxs[2]];
-    idxs[nonzeroIdx1] = 0;
-    idxs[nonzeroIdx2] = 1;
+    idxs[nonzeroIdx1] = COS;
+    idxs[nonzeroIdx2] = SIN;
     double B = fourierValues[idxs[0]][idxs[1]][idxs[2]];
-    idxs[nonzeroIdx1] = 1;
-    idxs[nonzeroIdx2] = 0;
+    idxs[nonzeroIdx1] = SIN;
+    idxs[nonzeroIdx2] = COS;
     double C = fourierValues[idxs[0]][idxs[1]][idxs[2]];
-    idxs[nonzeroIdx1] = 1;
-    idxs[nonzeroIdx2] = 1;
+    idxs[nonzeroIdx1] = SIN;
+    idxs[nonzeroIdx2] = SIN;
     double D = fourierValues[idxs[0]][idxs[1]][idxs[2]];
 
     if (A*A + B*B + C*C + D*D < AMPLITUDE_EPSILON*AMPLITUDE_EPSILON)
         return {};
 
-    double a = std::atan2(C + B, A - D);
-    double b = std::atan2(C - B, A + D);
+    double a = FourierTracker::guardedAtan2(C + B, A - D);
+    double b = FourierTracker::guardedAtan2(C - B, A + D);
     auto wavenumber1 = static_cast<double>(this->wavenumbers[nonzeroIdx1]);
     auto wavenumber2 = static_cast<double>(this->wavenumbers[nonzeroIdx2]);
     Vector<3> relPos{};
@@ -80,10 +78,10 @@ Vector<3> FourierTracker::calculateRelativeOriginPos2D(const FourierTracker::Fou
     return relPos;
 }
 
-FourierTracker::FourierValues FourierTracker::calculateFourierValues(const Packing &packing,
-                                                                     const ShapeTraits &shapeTraits) const
+FourierTracker::FourierCoefficients FourierTracker::calculateFourierCoefficients(const Packing &packing,
+                                                                                 const ShapeTraits &shapeTraits) const
 {
-    FourierValues fourierValues{};
+    FourierCoefficients coefficients{};
 
     for (std::size_t i{}; i < this->fourierFunctions[0].size(); i++) {
         for (std::size_t j{}; j < this->fourierFunctions[1].size(); j++) {
@@ -91,24 +89,24 @@ FourierTracker::FourierValues FourierTracker::calculateFourierValues(const Packi
                 const auto &f0 = this->fourierFunctions[0][i];
                 const auto &f1 = this->fourierFunctions[1][j];
                 const auto &f2 = this->fourierFunctions[2][k];
-                auto &fourierValue = fourierValues[i][j][k];
+                auto &coefficient = coefficients[i][j][k];
 
                 auto fProduct = [&f0, &f1, &f2](const Vector<3> &pos) {
                     return f0(pos[0]) * f1(pos[1]) * f2(pos[2]);
                 };
 
-                fourierValue = 0;
+                coefficient = 0;
                 for (std::size_t shapeI{}; shapeI < packing.size(); shapeI++) {
                     const auto &shape = packing[shapeI];
                     double functionMapping = this->function(shape, shapeTraits);
                     Vector<3> relativePos = packing.getBox().absoluteToRelative(shape.getPosition());
-                    fourierValue += fProduct(relativePos) * functionMapping;
+                    coefficient += fProduct(relativePos) * functionMapping;
                 }
             }
         }
     }
 
-    return fourierValues;
+    return coefficients;
 }
 
 FourierTracker::FourierTracker(const std::array<std::size_t, 3> &wavenumbers, Function function,
@@ -118,11 +116,15 @@ FourierTracker::FourierTracker(const std::array<std::size_t, 3> &wavenumbers, Fu
     for (std::size_t i{}; i < 3; i++) {
         std::size_t wavenumber = this->wavenumbers[i];
         if (wavenumber > 0)
-            this->nonzeroIdxs.push_back(i);
+            this->nonzeroWavenumberIdxs.push_back(i);
     }
-    Expects(!this->nonzeroIdxs.empty());
-    ExpectsMsg(this->nonzeroIdxs.size() != 3, "3D FourierTracker is not implemented yet");
+    Expects(!this->nonzeroWavenumberIdxs.empty());
+    ExpectsMsg(this->nonzeroWavenumberIdxs.size() != 3, "3D FourierTracker is not implemented yet");
 
+    this->fillFourierFunctions();
+}
+
+void FourierTracker::fillFourierFunctions() {
     auto functionFiller = [](std::size_t wavenumber) -> std::vector<FourierFunction> {
         if (wavenumber == 0) {
             FourierFunction constant = [](double) -> double { return 1; };
@@ -141,11 +143,12 @@ FourierTracker::FourierTracker(const std::array<std::size_t, 3> &wavenumbers, Fu
 Vector<3> FourierTracker::normalizeOriginPos(const Vector<3> &originPosRel) {
     PeriodicBoundaryConditions pbc(1);
     Vector<3> bestOriginPosRel = originPosRel;
-    double deltaR2 = pbc.getDistance2(this->previousRelValue, originPosRel);
+    double bestDeltaR2 = pbc.getDistance2(this->previousRelValue, originPosRel);
     std::array<std::size_t, 3> idxs{};
     for (idxs[0] = 0; idxs[0] < std::max(2*this->wavenumbers[0], 1ul); idxs[0]++) {
         for (idxs[1] = 0; idxs[1] < std::max(2*this->wavenumbers[1], 1ul); idxs[1]++) {
             for (idxs[2] = 0; idxs[2] < std::max(2*this->wavenumbers[2], 1ul); idxs[2]++) {
+                // Only "even" indices move from maximum to maximum ("odd" ones move to minimum)
                 if (std::accumulate(idxs.begin(), idxs.end(), 0ul) % 2 != 0)
                     continue;
 
@@ -155,8 +158,8 @@ Vector<3> FourierTracker::normalizeOriginPos(const Vector<3> &originPosRel) {
                         nextOriginPosRel[i] += static_cast<double>(idxs[i])/static_cast<double>(2*this->wavenumbers[i]);
 
                 double newDeltaR2 = pbc.getDistance2(this->previousRelValue, nextOriginPosRel);
-                if (newDeltaR2 < deltaR2) {
-                    deltaR2 = newDeltaR2;
+                if (newDeltaR2 < bestDeltaR2) {
+                    bestDeltaR2 = newDeltaR2;
                     bestOriginPosRel = nextOriginPosRel;
                 }
             }
@@ -165,4 +168,11 @@ Vector<3> FourierTracker::normalizeOriginPos(const Vector<3> &originPosRel) {
 
     this->previousRelValue = bestOriginPosRel;
     return bestOriginPosRel;
+}
+
+double FourierTracker::guardedAtan2(double y, double x) {
+    if (x*x + y*y < AMPLITUDE_EPSILON*AMPLITUDE_EPSILON)
+        return 0;
+    else
+        return std::atan2(y, x);
 }
