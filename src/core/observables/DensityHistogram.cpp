5569d761d69f0936aefbec39fad9bfdf43a93de3
Piotr Kubala <pkua.log@gmail.com>
2022-10-31 21:43:55 +0100

    DensityHistogram bulk observable

---
+++ src/core/observables/DensityHistogram.cpp
@@ -0,0 +1,57 @@
+//
+// Created by pkua on 31.10.22.
+//
+
+#include <algorithm>
+
+#include "DensityHistogram.h"
+#include "utils/Assertions.h"
+
+
+void DensityHistogram::addSnapshot(const Packing &packing, [[maybe_unused]] double temperature,
+                                   [[maybe_unused]] double pressure, const ShapeTraits &shapeTraits)
+{
+    this->tracker->calculateOrigin(packing, shapeTraits);
+    Vector<3> trackedOrigin = this->tracker->getOriginPos();
+    if (!this->firstOrigin.has_value())
+        this->firstOrigin = trackedOrigin;
+    Vector<3> originDelta = trackedOrigin - *this->firstOrigin;
+
+    const auto &box = packing.getBox();
+    const auto &bc = packing.getBoundaryConditions();
+    double volume = packing.getVolume();
+    #pragma omp parallel for shared(packing, bc, box, histogramBuilder, originDelta, volume) default(none)
+    for (std::size_t i = 0; i < packing.size(); i++) {
+        Vector<3> pos = packing[i].getPosition();
+        pos -= originDelta;
+        pos += bc.getCorrection(pos);
+        this->histogramBuilder.add(box.absoluteToRelative(pos), volume);
+    }
+    this->histogramBuilder.nextSnapshot();
+}
+
+void DensityHistogram::print(std::ostream &out) const {
+    for (auto [binMiddle, value] : this->histogramBuilder.dumpValues(HistogramBuilder3D::ReductionMethod::SUM))
+        out << binMiddle[0] << " " << binMiddle[1] << " " << binMiddle[2] << " " << value << std::endl;
+}
+
+void DensityHistogram::clear() {
+    this->histogramBuilder.clear();
+    this->tracker->reset();
+    this->firstOrigin = std::nullopt;
+}
+
+DensityHistogram::DensityHistogram(const std::array<std::size_t, 3> &numBins, std::unique_ptr<GoldstoneTracker> tracker,
+                                   std::size_t numThreads)
+        : numBins{normalizeNumBins(numBins)}, tracker{std::move(tracker)},
+        histogramBuilder({0, 0, 0}, {1, 1, 1}, this->numBins, numThreads)
+{
+    Expects(std::any_of(numBins.begin(), numBins.end(), [](std::size_t n) { return n > 1; }));
+}
+
+std::array<std::size_t, 3> DensityHistogram::normalizeNumBins(std::array<std::size_t, 3> array) {
+    for (auto &elem : array)
+        if (elem == 0)
+            elem = 1;
+    return array;
+}
