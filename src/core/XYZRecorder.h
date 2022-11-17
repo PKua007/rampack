adae4dc68c77292cf87398d1f8834e7745f9fc74
Piotr Kubala <pkua.log@gmail.com>
2022-11-17 18:21:16 +0100

    XYZRecorder

---
+++ src/core/XYZRecorder.h
@@ -0,0 +1,27 @@
+//
+// Created by pkua on 17.11.22.
+//
+
+#ifndef RAMPACK_XYZRECORDER_H
+#define RAMPACK_XYZRECORDER_H
+
+#include <ostream>
+
+#include "SimulationRecorder.h"
+
+
+class XYZRecorder : public SimulationRecorder {
+private:
+    std::unique_ptr<std::ostream> out;
+
+public:
+    explicit XYZRecorder(std::unique_ptr<std::ostream> out) : out{std::move(out)} { }
+    ~XYZRecorder() override = default;
+
+    void recordSnapshot(const Packing &packing, std::size_t cycle) override;
+
+    void close() override { this->out = nullptr; }
+};
+
+
+#endif //RAMPACK_XYZRECORDER_H
