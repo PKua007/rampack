98fc821e51ee9890e24557966ed752bb43bad7a3
Piotr Kubala <pkua.log@gmail.com>
2022-11-18 13:31:04 +0100

    XYZWriter

---
+++ src/core/XYZWriter.h
@@ -0,0 +1,18 @@
+//
+// Created by pkua on 18.11.22.
+//
+
+#ifndef RAMPACK_XYZWRITER_H
+#define RAMPACK_XYZWRITER_H
+
+#include "SnapshotWriter.h"
+
+
+class XYZWriter : public SnapshotWriter {
+public:
+    void write(std::ostream &out, const Packing &packing, const ShapeTraits &traits,
+               const std::map<std::string, std::string> &auxInfo) const override;
+};
+
+
+#endif //RAMPACK_XYZWRITER_H
