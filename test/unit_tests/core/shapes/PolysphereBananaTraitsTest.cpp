84b8ebbe985a96d9cce8846733b1394742aa62b5
Piotr Kubala <pkua.log@gmail.com>
2022-11-26 21:38:35 +0100

    PolysphereBananaTraits consistency update

--- test/unit_tests/core/shapes/PolysphereBananaTraitsTest.cpp
+++ test/unit_tests/core/shapes/PolysphereBananaTraitsTest.cpp
@@ -8,58 +8,39 @@
 
 #include "core/shapes/PolysphereBananaTraits.h"
 
-TEST_CASE("PolysphereBananaTraits") {
-    SECTION("odd number of spheres") {
-        PolysphereBananaTraits traits(2, M_PI/2, 3, 1, false);
 
-        SECTION("sphere data") {
-            const auto &data = traits.getSphereData();
-            REQUIRE(data.size() == 3);
-            CHECK(data[0].radius == 1);
-            CHECK(data[1].radius == 1);
-            CHECK(data[2].radius == 1);
-            CHECK_THAT(data[0].position, IsApproxEqual({2 - M_SQRT2, -M_SQRT2, 0}, 1e-12));
-            CHECK_THAT(data[1].position, IsApproxEqual({0, 0, 0}, 1e-12));
-            CHECK_THAT(data[2].position, IsApproxEqual({2 - M_SQRT2, +M_SQRT2, 0}, 1e-12));
+TEST_CASE("PolysphereBananaTraits") {
+    SECTION("below half-angle") {
+        PolysphereBananaTraits traits(2, 2*M_PI/3, 3, 1);
+
+        SECTION("spehre data") {
+            const auto &sphereData = traits.getSphereData();
+            REQUIRE(sphereData.size() == 3);
+            CHECK_THAT(sphereData[0].position, IsApproxEqual({0, 0, -std::sqrt(3)}, 1e-12));
+            CHECK_THAT(sphereData[1].position, IsApproxEqual({-1, 0, 0}, 1e-12));
+            CHECK_THAT(sphereData[2].position, IsApproxEqual({0, 0, +std::sqrt(3)}, 1e-12));
+            for (const auto &data: sphereData)
+                CHECK(data.radius == 1);
         }
 
         SECTION("geometry") {
-            const auto &geometry = traits.getGeometry();
-
-            CHECK_THAT(geometry.getPrimaryAxis({}), IsApproxEqual({0, 1, 0}, 1e-12));
-            CHECK_THAT(geometry.getSecondaryAxis({}), IsApproxEqual({-1, 0, 0}, 1e-12));
-            CHECK_THAT(geometry.getGeometricOrigin({}), IsApproxEqual({4./3 - 2*M_SQRT2/3, 0, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("s0", {}), IsApproxEqual({2 - M_SQRT2, -M_SQRT2, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("beg", {}), IsApproxEqual({2 - M_SQRT2, -M_SQRT2, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("s2", {}), IsApproxEqual({2 - M_SQRT2, +M_SQRT2, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("end", {}), IsApproxEqual({2 - M_SQRT2, +M_SQRT2, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("o", {}), IsApproxEqual({4. / 3 - 2 * M_SQRT2 / 3, 0, 0}, 1e-12));
-            CHECK_THAT(geometry.getNamedPointForShape("cm", {}), IsApproxEqual({0, 0, 0}, 1e-12));
+            const auto &geom = traits.getGeometry();
+            CHECK_THAT(geom.getPrimaryAxis({}), IsApproxEqual({0, 0, 1}, 1e-12));
+            CHECK_THAT(geom.getSecondaryAxis({}), IsApproxEqual({-1, 0, 0}, 1e-12));
+            CHECK_THAT(geom.getNamedPoint("beg"), IsApproxEqual({0, 0, -std::sqrt(3)}, 1e-12));
+            CHECK_THAT(geom.getNamedPoint("end"), IsApproxEqual({0, 0, +std::sqrt(3)}, 1e-12));
         }
     }
 
-    SECTION("odd number of spheres (normalized)") {
-        PolysphereBananaTraits traits(2, M_PI/2, 3, 1, true);
+    SECTION("above half-angle") {
+        PolysphereBananaTraits traits(2, 4*M_PI/3, 3, 1);
 
-        const auto &data = traits.getSphereData();
-        REQUIRE(data.size() == 3);
-        CHECK_THAT(data[1].position, IsApproxEqual({-4./3 + 2*M_SQRT2/3, 0, 0}, 1e-12));
-        CHECK(traits.getGeometry().getGeometricOrigin({}) == Vector<3>{}); // Strictly equal!
-    }
-
-    SECTION("even number of spheres") {
-        PolysphereBananaTraits traits(2, 3*M_PI/4, 4, 1, false);
-
-        const auto &data = traits.getSphereData();
-        REQUIRE(data.size() == 4);
-        CHECK(data[0].radius == 1);
-        CHECK(data[1].radius == 1);
-        CHECK(data[2].radius == 1);
-        CHECK(data[3].radius == 1);
-        CHECK_THAT(data[0].position, IsApproxEqual({2, -2, 0}, 1e-12));
-        CHECK_THAT(data[1].position, IsApproxEqual({2 - M_SQRT2, -M_SQRT2, 0}, 1e-12));
-        CHECK_THAT(data[2].position, IsApproxEqual({0, 0, 0}, 1e-12));
-        CHECK_THAT(data[3].position, IsApproxEqual({2 - M_SQRT2, +M_SQRT2, 0}, 1e-12));
-        CHECK_THAT(traits.getGeometry().getGeometricOrigin({}), IsApproxEqual({1.5-0.5*M_SQRT2, -0.5, 0}, 1e-12));
+        const auto &sphereData = traits.getSphereData();
+        REQUIRE(sphereData.size() == 3);
+        CHECK_THAT(sphereData[0].position, IsApproxEqual({1, 0, -std::sqrt(3)}, 1e-12));
+        CHECK_THAT(sphereData[1].position, IsApproxEqual({-2, 0, 0}, 1e-12));
+        CHECK_THAT(sphereData[2].position, IsApproxEqual({1, 0, +std::sqrt(3)}, 1e-12));
+        for (const auto &data : sphereData)
+            CHECK(data.radius == 1);
     }
 }
