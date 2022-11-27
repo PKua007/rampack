eb62e07c221444b3977250ea20d6ea2fcd9680bc
Piotr Kubala <pkua.log@gmail.com>
2022-11-27 18:11:14 +0100

    PolysphereWedgeTraits consistency update

--- src/core/shapes/PolysphereWedgeTraits.cpp
+++ src/core/shapes/PolysphereWedgeTraits.cpp
@@ -5,33 +5,40 @@
 #include "PolysphereWedgeTraits.h"
 #include "utils/Assertions.h"
 
+
 PolysphereWedgeTraits::PolysphereGeometry
-PolysphereWedgeTraits::generateGeometry(std::size_t sphereNum, double smallSphereRadius, double largeSphereRadius,
-                                        double spherePenetration, bool normalizeMassCentre)
+PolysphereWedgeTraits::generateGeometry(std::size_t sphereNum, double bottomSphereRadius, double topSphereRadius,
+                                        double spherePenetration)
 {
     Expects(sphereNum >= 2);
-    Expects(smallSphereRadius > 0);
-    Expects(largeSphereRadius > 0);
-    Expects(spherePenetration < 2*std::min(smallSphereRadius, largeSphereRadius));
+    Expects(topSphereRadius > 0);
+    Expects(bottomSphereRadius > 0);
+    Expects(spherePenetration < 2*std::min(topSphereRadius, bottomSphereRadius));
 
     std::vector<SphereData> data;
 
     double centrePos{};
-    double radiusDelta = (largeSphereRadius - smallSphereRadius)/static_cast<double>(sphereNum - 1);
-    double currentRadius = smallSphereRadius;
+    double radiusDelta = (topSphereRadius - bottomSphereRadius) / static_cast<double>(sphereNum - 1);
+    double currentRadius = bottomSphereRadius;
     for (std::size_t i{}; i < sphereNum; i++) {
-        data.emplace_back(Vector<3>{centrePos, 0, 0}, currentRadius);
+        data.emplace_back(Vector<3>{0, 0, centrePos}, currentRadius);
         centrePos += 2*currentRadius + radiusDelta - spherePenetration;
         currentRadius += radiusDelta;
     }
 
-    PolysphereGeometry geometry(std::move(data), {1, 0, 0}, {0, 1, 0}, {0, 0, 0});
-    if (normalizeMassCentre)
-        geometry.normalizeMassCentre();
-    double end1 = geometry.getSphereData().front().position[0];
-    double end2 = geometry.getSphereData().back().position[0];
-    geometry.setGeometricOrigin({(end2 + largeSphereRadius + end1 - smallSphereRadius)/2, 0, 0});
-    geometry.addCustomNamedPoints({{"ss", geometry.getSphereData().front().position},
-                                   {"sl", geometry.getSphereData().back().position}});
+    {
+        double beg = data.front().position[2] - data.front().radius;
+        double end = data.back().position[2] + data.back().radius;
+        double origin = (beg + end)/2;
+        Vector<3> translation{0, 0, -origin};
+        std::vector<SphereData> newData;
+        newData.reserve(data.size());
+        for (const auto &dataElem : data)
+            newData.emplace_back(dataElem.position + translation, dataElem.radius);
+        std::swap(data, newData);
+    }
+
+    PolysphereGeometry geometry(data, {0, 0, 1}, {1, 0, 0}, {0, 0, 0});
+    geometry.addCustomNamedPoints({{"beg", data.front().position}, {"end", data.back().position}});
     return geometry;
 }
--- test/unit_tests/core/shapes/PolysphereWedgeTraitsTest.cpp
+++ test/unit_tests/core/shapes/PolysphereWedgeTraitsTest.cpp
@@ -11,34 +11,32 @@
 
 TEST_CASE("PolysphereWedgeTraits") {
     std::size_t sphereNum = 3;
-    double smallSphereRadius = 2;
-    double largeSphereRadius = 4;
+    double bottomSphereRadius = 4;
+    double topSphereRadius = 2;
     double spherePenetration = 1;
-    PolysphereWedgeTraits traits(sphereNum, smallSphereRadius, largeSphereRadius, spherePenetration, false);
+    PolysphereWedgeTraits traits(sphereNum, bottomSphereRadius, topSphereRadius, spherePenetration);
 
     SECTION("sphere data") {
         const auto &data = traits.getSphereData();
         REQUIRE(data.size() == 3);
-        CHECK(data[0].radius == 2);
+        CHECK(data[0].radius == 4);
         CHECK(data[1].radius == 3);
-        CHECK(data[2].radius == 4);
-        CHECK_THAT(data[0].position, IsApproxEqual({0, 0, 0}, 1e-12));
-        CHECK_THAT(data[1].position, IsApproxEqual({4, 0, 0}, 1e-12));
-        CHECK_THAT(data[2].position, IsApproxEqual({10, 0, 0}, 1e-12));
+        CHECK(data[2].radius == 2);
+        CHECK_THAT(data[0].position, IsApproxEqual({0, 0, -4}, 1e-12));
+        CHECK_THAT(data[1].position, IsApproxEqual({0, 0, 2}, 1e-12));
+        CHECK_THAT(data[2].position, IsApproxEqual({0, 0, 6}, 1e-12));
     }
 
     SECTION("geometry") {
         const auto &geometry = traits.getGeometry();
-        Shape shape({1, 2, 3}, Matrix<3, 3>::rotation(0, 0, M_PI/2));
 
-        CHECK_THAT(geometry.getGeometricOrigin(shape), IsApproxEqual({0, 6, 0}, 1e-12));
-        CHECK_THAT(geometry.getPrimaryAxis(shape), IsApproxEqual({0, 1, 0}, 1e-12));
-        CHECK_THAT(geometry.getSecondaryAxis(shape), IsApproxEqual({-1, 0, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("s0", shape), IsApproxEqual(Vector<3>{1, 2, 3} + Vector<3>{0, 0, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("ss", shape), IsApproxEqual(Vector<3>{1, 2, 3} + Vector<3>{0, 0, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("s2", shape), IsApproxEqual(Vector<3>{1, 2, 3} + Vector<3>{0, 10, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("sl", shape), IsApproxEqual(Vector<3>{1, 2, 3} + Vector<3>{0, 10, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("o", shape), IsApproxEqual(Vector<3>{1, 2, 3} + Vector<3>{0, 6, 0}, 1e-12));
-        CHECK_THAT(geometry.getNamedPointForShape("cm", shape), IsApproxEqual({1, 2, 3}, 1e-12));
+        CHECK_THAT(geometry.getGeometricOrigin({}), IsApproxEqual({0, 0, 0}, 1e-12));
+        CHECK_THAT(geometry.getPrimaryAxis({}), IsApproxEqual({0, 0, 1}, 1e-12));
+        CHECK_THAT(geometry.getSecondaryAxis({}), IsApproxEqual({1, 0, 0}, 1e-12));
+        CHECK_THAT(geometry.getNamedPoint("s0"), IsApproxEqual({0, 0, -4}, 1e-12));
+        CHECK_THAT(geometry.getNamedPoint("beg"), IsApproxEqual({0, 0, -4}, 1e-12));
+        CHECK_THAT(geometry.getNamedPoint("s2"), IsApproxEqual({0, 0, 6}, 1e-12));
+        CHECK_THAT(geometry.getNamedPoint("end"), IsApproxEqual({0, 0, 6}, 1e-12));
+        CHECK_THAT(geometry.getNamedPoint("o"), IsApproxEqual({0, 0, 0}, 1e-12));
     }
 }
