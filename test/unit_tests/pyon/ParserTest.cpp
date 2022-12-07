6b8119592be2d756d02448086a8d4da5b7a27e45
Piotr Kubala <pkua.log@gmail.com>
2022-12-07 01:47:17 +0100

    The rest of literals

--- test/unit_tests/pyon/ParserTest.cpp
+++ test/unit_tests/pyon/ParserTest.cpp
@@ -20,4 +20,47 @@ TEST_CASE("Parser: literals") {
         auto node = Parser::parse("1.2e-4");
         CHECK(node->as<NodeFloat>()->getValue() == 1.2e-4);
     }
+
+    SECTION("boolean") {
+        auto trueNode = Parser::parse("True");
+        CHECK(trueNode->as<NodeBoolean>()->getValue());
+        auto falseNode = Parser::parse("False");
+        CHECK_FALSE(falseNode->as<NodeBoolean>()->getValue());
+    }
+
+    SECTION("none") {
+        auto node = Parser::parse("None");
+        CHECK(node->getType() == Node::NONE);
+    }
+
+    SECTION("string") {
+        SECTION("empty") {
+            auto node = Parser::parse(R"("")");
+            CHECK(node->as<NodeString>()->getValue().empty());
+        }
+
+        SECTION("easy") {
+            auto node = Parser::parse(R"("abc")");
+            CHECK(node->as<NodeString>()->getValue() == "abc");
+        }
+
+        SECTION("escaped") {
+            auto node = Parser::parse(R"("a\n\tb\\")");
+            CHECK(node->as<NodeString>()->getValue() == "a\n\tb\\");
+        }
+
+        SECTION("errors") {
+            SECTION("EOF") {
+                CHECK_THROWS_AS(Parser::parse(R"("abc)"), ParseException);
+            }
+
+            SECTION("EOF after backslash") {
+                CHECK_THROWS_AS(Parser::parse(R"("abc\)"), ParseException);
+            }
+
+            SECTION("unknown escaped char") {
+                CHECK_THROWS_AS(Parser::parse(R"("abc\x")"), ParseException);
+            }
+        }
+    }
 }
