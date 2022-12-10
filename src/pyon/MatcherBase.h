4db375c7964194dba95860408f33f6a54f78d398
Piotr Kubala <pkua.log@gmail.com>
2022-12-10 15:40:47 +0100

    Pyon MatcherBase

---
+++ src/pyon/MatcherBase.h
@@ -0,0 +1,23 @@
+//
+// Created by pkua on 10.12.22.
+//
+
+#ifndef RAMPACK_MATCHERBASE_H
+#define RAMPACK_MATCHERBASE_H
+
+#include <memory>
+
+#include "Any.h"
+#include "Node.h"
+
+
+namespace pyon::matcher {
+    class MatcherBase {
+    public:
+        virtual ~MatcherBase() = default;
+
+        virtual Any match(std::shared_ptr<const ast::Node> node) = 0;
+    };
+} // matcher
+
+#endif //RAMPACK_MATCHERBASE_H
