eb5128061d4dd7dae7996ed084d0c7ecc696ec31
Piotr Kubala <pkua.log@gmail.com>
2022-12-23 19:34:55 +0100

    MatcherAlternative::outline

--- src/pyon/MatcherAlternative.cpp
+++ src/pyon/MatcherAlternative.cpp
@@ -2,7 +2,10 @@
 // Created by Piotr Kubala on 15/12/2022.
 //
 
+#include <iomanip>
+
 #include "MatcherAlternative.h"
+
 
 namespace pyon::matcher {
     bool MatcherAlternative::match(std::shared_ptr<const ast::Node> node, Any &result) const {
@@ -11,5 +14,23 @@ namespace pyon::matcher {
                 return true;
 
         return false;
+    }
+
+    std::string MatcherAlternative::outline(std::size_t indent) const {
+        std::string spaces(indent, ' ');
+        std::ostringstream out;
+
+        out << spaces << "Alternative:";
+        std::size_t numberWidth = std::to_string(this->alternatives.size()).length();
+        for (std::size_t i{}; i < this->alternatives.size(); i++) {
+            const MatcherBase &alternative = *this->alternatives[i];
+            std::string alternativeOutline = alternative.outline(indent + numberWidth + 2);
+            alternativeOutline = alternativeOutline.substr(indent + numberWidth + 2);
+            std::string number = std::to_string(i + 1) + ".";
+            out << std::endl << spaces << std::setw(static_cast<int>(numberWidth + 2)) << std::left << number;
+            out << alternativeOutline;
+        }
+
+        return out.str();
     }
 } // matcher
