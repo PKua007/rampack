212b9480bd074147439c558bb66fced20e078cfd
Piotr Kubala <pkua.log@gmail.com>
2023-01-01 19:12:59 +0100

    Nematic and smectic order in new format

--- src/frontend/ObservablesMatcher.cpp
+++ src/frontend/ObservablesMatcher.cpp
@@ -11,6 +11,9 @@
 #include "core/observables/CompressibilityFactor.h"
 #include "core/observables/EnergyPerParticle.h"
 #include "core/observables/EnergyFluctuationsPerParticle.h"
+#include "core/observables/NematicOrder.h"
+#include "core/observables/SmecticOrder.h"
+
 
 using namespace pyon::matcher;
 
@@ -31,6 +34,8 @@ namespace {
     MatcherDataclass create_compressibility_factor();
     MatcherDataclass create_energy_per_particle();
     MatcherDataclass create_energy_fluctuations_per_particle();
+    MatcherDataclass create_nematic_order();
+    MatcherDataclass create_smectic_order();
 
 
     MatcherAlternative create_observable_matcher(std::size_t maxThreads) {
@@ -39,7 +44,9 @@ namespace {
             | create_packing_fraction()
             | create_compressibility_factor()
             | create_energy_per_particle()
-            | create_energy_fluctuations_per_particle();
+            | create_energy_fluctuations_per_particle()
+            | create_nematic_order()
+            | create_smectic_order();
     }
 
     MatcherAlternative create_scoped_observable_matcher(std::size_t maxThreads) {
@@ -111,6 +118,47 @@ namespace {
             return std::make_pair(FULL_SCOPE, std::make_shared<EnergyFluctuationsPerParticle>());
         });
     }
+
+    MatcherDataclass create_nematic_order() {
+        return MatcherDataclass("nematic_order")
+            .arguments({{"dump_qtensor", MatcherBoolean{}, "False"}})
+            .mapTo([](const DataclassData &nematicOrder) -> ObservableData {
+                auto dumpQTensor = nematicOrder["dump_qtensor"].as<bool>();
+                return std::make_pair(FULL_SCOPE, std::make_shared<NematicOrder>(dumpQTensor));
+            });
+    }
+
+    MatcherDataclass create_smectic_order() {
+        auto singleHkl = MatcherInt{}
+            .positive()
+            .mapTo([](long i) {
+                auto iUl = static_cast<std::size_t>(i);
+                return std::array<std::size_t, 3>{iUl, iUl, iUl};
+            });
+        auto allHkl = MatcherArray{}
+            .elementsMatch(MatcherInt{}.nonNegative().mapTo<std::size_t>())
+            .size(3)
+            .filter([](const ArrayData &array) {
+                return std::any_of(array.begin(), array.end(), [](const Any &any) {
+                    return any.as<std::size_t>() > 0;
+                });
+            })
+            .describe("with at least one non-zero index")
+            .mapToStdArray<std::size_t, 3>();
+        auto hkl = singleHkl | allHkl;
+
+        return MatcherDataclass("smectic_order")
+            .arguments({{"max_hkl", hkl},
+                        {"dump_tau_vector", MatcherBoolean{}, "False"},
+                        {"focal_point", MatcherString{}.nonEmpty(), R"("o")"}})
+            .mapTo([](const DataclassData &smecticOrder) -> ObservableData {
+                auto maxHkl = smecticOrder["max_hkl"].as<std::array<std::size_t, 3>>();
+                auto dumpTauVector = smecticOrder["dump_tau_vector"].as<bool>();
+                auto focalPoint = smecticOrder["focal_point"].as<std::string>();
+                auto observable = std::make_shared<SmecticOrder>(maxHkl, dumpTauVector, focalPoint);
+                return std::make_pair(FULL_SCOPE, observable);
+            });
+    }
 }
 
 
@@ -118,7 +166,7 @@ MatcherArray ObservableMatcher::createObservablesMatcher(std::size_t maxThreads)
     if (maxThreads == 0)
         maxThreads = OMP_MAXTHREADS;
 
-    return MatcherArray()
+    return MatcherArray{}
         .elementsMatch(create_scoped_observable_matcher(maxThreads))
         .mapToStdVector<ObservableData>();
 }
@@ -127,5 +175,5 @@ MatcherArray ObservableMatcher::createBulkObservablesMatcher(std::size_t maxThre
     if (maxThreads == 0)
         maxThreads = OMP_MAXTHREADS;
 
-    return {};
+    return MatcherArray{}.mapToStdVector<std::shared_ptr<BulkObservable>>();
 }
