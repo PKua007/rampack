bee95e17e2011e3d91a9cb18553ad40d58a91ba6
Piotr Kubala <pkua.log@gmail.com>
2023-01-03 20:41:58 +0100

    Presimulated arrangement in new format

--- src/frontend/ArrangementMatcher.h
+++ src/frontend/ArrangementMatcher.h
@@ -14,7 +14,9 @@ public:
     class PackingFactory {
     public:
         virtual ~PackingFactory() = default;
-        virtual std::unique_ptr<Packing> createPacking() = 0;
+        virtual std::unique_ptr<Packing> createPacking(std::unique_ptr<BoundaryConditions> bc,
+                                                       const Interaction &interaction, std::size_t moveThreads,
+                                                       std::size_t scalingThreads) = 0;
     };
 
     static pyon::matcher::MatcherAlternative create();
