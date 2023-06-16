242c3c11a2f9e5530bdedbc9a2e1f81068115073
Piotr Kubala <pkua.log@gmail.com>
2023-06-16 17:21:50 +0200

    GCC OMP [[maybe_unused]] warning fixed

--- src/utils/OMPMacros.h
+++ src/utils/OMPMacros.h
@@ -10,10 +10,12 @@
     #define OMP_MAXTHREADS              omp_get_max_threads()
     #define OMP_SET_NUM_THREADS(num)    omp_set_num_threads(num)
     #define OMP_THREAD_ID               omp_get_thread_num()
+    #define OMP_MAYBE_UNUSED
 #else
     #define OMP_MAXTHREADS              1
     #define OMP_SET_NUM_THREADS(num)    static_cast<void>(num)
     #define OMP_THREAD_ID               0
+    #define OMP_MAYBE_UNUSED            [[maybe_unused]]
 #endif
 
 #endif //MBL_ED_OMPMACROS_H
