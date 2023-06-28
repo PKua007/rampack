8778d0c8dd532f081461fa03d4a8eea9a9923174
Piotr Kubala <pkua.log@gmail.com>
2023-06-28 23:27:02 +0200

    Relative path in contract exceptions

--- src/utils/Exceptions.cpp
+++ src/utils/Exceptions.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <sstream>
+#include <filesystem>
 
 #include "Exceptions.h"
 
@@ -10,9 +11,12 @@
 std::string ContractException::makeWhat(const std::string &file_, const std::string &function_, std::size_t line_,
                                         const std::string &condition_, const std::string &message_)
 {
+    std::string relativePath = std::filesystem::relative(file_, RAMPACK_ROOT_DIR);
+    relativePath = "<project root>/" + relativePath;
+
     std::ostringstream out;
     out << "Contract violated!" << std::endl;
-    out << "Location         : " << file_ << ":" << line_ << " (function \"" << function_ << "\"):" << std::endl;
+    out << "Location         : " << relativePath << ":" << line_ << " (function \"" << function_ << "\"):" << std::endl;
     out << "Failed condition : " << condition_ << std::endl;
     out << "Message          : " << message_;
     return out.str();
