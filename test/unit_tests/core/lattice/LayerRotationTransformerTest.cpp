08348101ad6f462c76e072ae3ace9f25b093488d
Piotr Kubala <pkua.log@gmail.com>
2026-05-10 22:41:48 +0200

    Refactor LayerRotationTransformerTest

--- test/unit_tests/core/lattice/LayerRotationTransformerTest.cpp
+++ test/unit_tests/core/lattice/LayerRotationTransformerTest.cpp
@@ -10,13 +10,30 @@
 #include "core/lattice/LayerRotationTransformer.h"
 
 
+namespace {
+    constexpr bool isAlternating = true;
+    constexpr bool isNonAlternating = false;
+    constexpr bool isCumulative = true;
+    constexpr bool isNonCumulative = false;
+
+    void check_molecules(const std::vector<Shape> &molecules, const std::vector<Shape> &expectedMolecules) {
+        REQUIRE(molecules.size() == expectedMolecules.size());
+
+        for (std::size_t i = 0; i < molecules.size(); i++) {
+            CHECK(molecules[i].getPosition() == expectedMolecules[i].getPosition());
+            CHECK_THAT(molecules[i].getOrientation(), IsApproxEqual(expectedMolecules[i].getOrientation(), 1e-12));
+        }
+    }
+}
+
+
 TEST_CASE("LayerRotationTransformer: alternating") {
-    const auto [isCumulative, expectedEvenRotation, expectedOddRotation] = GENERATE(
-        std::make_tuple(false, Matrix<3, 3>::rotation(M_PI/2, 0, 0), Matrix<3, 3>::rotation(-M_PI/2, 0, 0)),
-        std::make_tuple(true, Matrix<3, 3>::identity(), Matrix<3, 3>::rotation(-M_PI/2, 0, 0))
+    const auto [isCumulativeMode, expectedEvenRotation, expectedOddRotation] = GENERATE(
+        std::make_tuple(isNonCumulative, Matrix<3, 3>::rotation(M_PI/2, 0, 0), Matrix<3, 3>::rotation(-M_PI/2, 0, 0)),
+        std::make_tuple(isCumulative, Matrix<3, 3>::identity(), Matrix<3, 3>::rotation(-M_PI/2, 0, 0))
     );
 
-    DYNAMIC_SECTION((isCumulative ? "cumulative" : "non-cumulative")) {
+    DYNAMIC_SECTION((isCumulativeMode ? "cumulative" : "non-cumulative")) {
         using RotationAngle = LayerRotationTransformer::RotationAngle;
         using FullRotationQuotient = LayerRotationTransformer::FullRotationQuotient;
         const auto [angleType, rotationAngle] = GENERATE(
@@ -29,21 +46,18 @@ TEST_CASE("LayerRotationTransformer: alternating") {
                               {Shape({0, 0.5, 0.25}), Shape({0.5, 0.5, 0.25}), Shape({0, 0.5, 0.75})});
             Lattice lattice(unitCell, {2, 2, 2});
             LayerRotationTransformer layerRotationTransformer(
-                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, rotationAngle, true, isCumulative
+                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, rotationAngle, isAlternating, isCumulativeMode
             );
             MockShapeTraits shapeTraits;
 
             layerRotationTransformer.transform(lattice, shapeTraits);
 
             CHECK(lattice.getDimensions() == std::array<std::size_t, 3>{2, 2, 2});
-            const auto &molecules = lattice.getSpecificCell(0, 0, 0).getMolecules();
-            REQUIRE(molecules.size() == 3);
-            CHECK(molecules[0].getPosition() == Vector<3>{0, 0.5, 0.25});
-            CHECK_THAT(molecules[0].getOrientation(), IsApproxEqual(expectedEvenRotation, 1e-12));
-            CHECK(molecules[1].getPosition() == Vector<3>{0.5, 0.5, 0.25});
-            CHECK_THAT(molecules[1].getOrientation(), IsApproxEqual(expectedEvenRotation, 1e-12));
-            CHECK(molecules[2].getPosition() == Vector<3>{0, 0.5, 0.75});
-            CHECK_THAT(molecules[2].getOrientation(), IsApproxEqual(expectedOddRotation, 1e-12));
+            check_molecules(lattice.getSpecificCell(0, 0, 0).getMolecules(), {
+                Shape({0, 0.5, 0.25}, expectedEvenRotation),
+                Shape({0.5, 0.5, 0.25}, expectedEvenRotation),
+                Shape({0, 0.5, 0.75}, expectedOddRotation)
+            });
         }
     }
 }
@@ -62,21 +76,19 @@ TEST_CASE("LayerRotationTransformer: non-alternating") {
                               {Shape({0, 0.5, 0.25}), Shape({0.5, 0.5, 0.25}), Shape({0, 0.5, 0.75})});
             Lattice lattice(unitCell, {2, 2, 2});
             LayerRotationTransformer layerRotationTransformer(
-                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, rotationAngle, false
+                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, rotationAngle, isNonAlternating, isNonCumulative
             );
             MockShapeTraits shapeTraits;
 
             layerRotationTransformer.transform(lattice, shapeTraits);
 
             CHECK(lattice.getDimensions() == std::array<std::size_t, 3>{2, 2, 2});
-            const auto &molecules = lattice.getSpecificCell(0, 0, 0).getMolecules();
-            REQUIRE(molecules.size() == 3);
-            CHECK(molecules[0].getPosition() == Vector<3>{0, 0.5, 0.25});
-            CHECK_THAT(molecules[0].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(M_PI/2, 0, 0), 1e-12));
-            CHECK(molecules[1].getPosition() == Vector<3>{0.5, 0.5, 0.25});
-            CHECK_THAT(molecules[1].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(M_PI/2, 0, 0), 1e-12));
-            CHECK(molecules[2].getPosition() == Vector<3>{0, 0.5, 0.75});
-            CHECK_THAT(molecules[2].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(M_PI/2, 0, 0), 1e-12));
+            const auto expectedRotation = Matrix<3, 3>::rotation(M_PI/2, 0, 0);
+            check_molecules(lattice.getSpecificCell(0, 0, 0).getMolecules(), {
+                Shape({0, 0.5, 0.25}, expectedRotation),
+                Shape({0.5, 0.5, 0.25}, expectedRotation),
+                Shape({0, 0.5, 0.75}, expectedRotation)
+            });
         }
     }
 
@@ -86,27 +98,21 @@ TEST_CASE("LayerRotationTransformer: non-alternating") {
                   {Shape({0.5, 0.5, 0.25}), Shape({0.5, 0.5, 0.5}), Shape({0.5, 0.5, 0.75})});
             Lattice lattice(unitCell, {2, 2, 2});
             LayerRotationTransformer layerRotationTransformer(
-                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, 2*M_PI/3, false, true
+                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, 2*M_PI/3, isNonAlternating, isCumulative
             );
             MockShapeTraits shapeTraits;
 
             layerRotationTransformer.transform(lattice, shapeTraits);
 
             CHECK(lattice.getDimensions() == std::array<std::size_t, 3>{2, 2, 1});
-            const auto &molecules = lattice.getSpecificCell(0, 0, 0).getMolecules();
-            REQUIRE(molecules.size() == 6);
-            CHECK(molecules[0].getPosition() == Vector<3>{0.5, 0.5, 0.125});
-            CHECK_THAT(molecules[0].getOrientation(), IsApproxEqual(Matrix<3, 3>::identity(), 1e-12));
-            CHECK(molecules[1].getPosition() == Vector<3>{0.5, 0.5, 0.25});
-            CHECK_THAT(molecules[1].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(2*M_PI/3, 0, 0), 1e-12));
-            CHECK(molecules[2].getPosition() == Vector<3>{0.5, 0.5, 0.375});
-            CHECK_THAT(molecules[2].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(4*M_PI/3, 0, 0), 1e-12));
-            CHECK(molecules[3].getPosition() == Vector<3>{0.5, 0.5, 0.625});
-            CHECK_THAT(molecules[3].getOrientation(), IsApproxEqual(Matrix<3, 3>::identity(), 1e-12));
-            CHECK(molecules[4].getPosition() == Vector<3>{0.5, 0.5, 0.75});
-            CHECK_THAT(molecules[4].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(2*M_PI/3, 0, 0), 1e-12));
-            CHECK(molecules[5].getPosition() == Vector<3>{0.5, 0.5, 0.875});
-            CHECK_THAT(molecules[5].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(4*M_PI/3, 0, 0), 1e-12));
+            check_molecules(lattice.getSpecificCell(0, 0, 0).getMolecules(), {
+                Shape({0.5, 0.5, 0.125}, Matrix<3, 3>::identity()),
+                Shape({0.5, 0.5, 0.25}, Matrix<3, 3>::rotation(2*M_PI/3, 0, 0)),
+                Shape({0.5, 0.5, 0.375}, Matrix<3, 3>::rotation(4*M_PI/3, 0, 0)),
+                Shape({0.5, 0.5, 0.625}, Matrix<3, 3>::identity()),
+                Shape({0.5, 0.5, 0.75}, Matrix<3, 3>::rotation(2*M_PI/3, 0, 0)),
+                Shape({0.5, 0.5, 0.875}, Matrix<3, 3>::rotation(4*M_PI/3, 0, 0))
+            });
         }
 
         SECTION("rotation angle as quotient") {
@@ -114,22 +120,19 @@ TEST_CASE("LayerRotationTransformer: non-alternating") {
                   {Shape({0.5, 0.5, 0.25}), Shape({0.5, 0.5, 0.5}), Shape({0.5, 0.5, 0.75})});
             Lattice lattice(unitCell, {2, 2, 2});
             using FullRotationQuotient = LayerRotationTransformer::FullRotationQuotient;
-            LayerRotationTransformer layerRotationTransformer(
-                LatticeTraits::Axis::Z, LatticeTraits::Axis::X, FullRotationQuotient(1, 3), false, true
-            );
+            LayerRotationTransformer layerRotationTransformer(LatticeTraits::Axis::Z, LatticeTraits::Axis::X,
+                                                              FullRotationQuotient(1, 3), isNonAlternating,
+                                                              isCumulative);
             MockShapeTraits shapeTraits;
 
             layerRotationTransformer.transform(lattice, shapeTraits);
 
             CHECK(lattice.getDimensions() == std::array<std::size_t, 3>{2, 2, 2});
-            const auto &molecules = lattice.getSpecificCell(0, 0, 0).getMolecules();
-            REQUIRE(molecules.size() == 3);
-            CHECK(molecules[0].getPosition() == Vector<3>{0.5, 0.5, 0.25});
-            CHECK_THAT(molecules[0].getOrientation(), IsApproxEqual(Matrix<3, 3>::identity(), 1e-12));
-            CHECK(molecules[1].getPosition() == Vector<3>{0.5, 0.5, 0.5});
-            CHECK_THAT(molecules[1].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(2*M_PI/3, 0, 0), 1e-12));
-            CHECK(molecules[2].getPosition() == Vector<3>{0.5, 0.5, 0.75});
-            CHECK_THAT(molecules[2].getOrientation(), IsApproxEqual(Matrix<3, 3>::rotation(4*M_PI/3, 0, 0), 1e-12));
+            check_molecules(lattice.getSpecificCell(0, 0, 0).getMolecules(), {
+                Shape({0.5, 0.5, 0.25}, Matrix<3, 3>::identity()),
+                Shape({0.5, 0.5, 0.5}, Matrix<3, 3>::rotation(2*M_PI/3, 0, 0)),
+                Shape({0.5, 0.5, 0.75}, Matrix<3, 3>::rotation(4*M_PI/3, 0, 0))
+            });
         }
     }
 }
