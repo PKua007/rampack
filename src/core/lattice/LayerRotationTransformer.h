6be682ac070033420ae28664ba2342c8a33090d7
Piotr Kubala <pkua.log@gmail.com>
2026-05-12 02:20:35 +0200

    Update doxygen doc in LayerRotationTransformer

--- src/core/lattice/LayerRotationTransformer.h
+++ src/core/lattice/LayerRotationTransformer.h
@@ -26,7 +26,9 @@ public:
      * [`gcd(numerator, denominator) = 1`].
      */
     struct FullRotationQuotient {
+        /** @brief Signed numerator of the quotient. */
         const int numerator{};
+        /** @brief Nonzero, unsigned denominator of the quotient. */
         const unsigned denominator{};
 
         FullRotationQuotient(int numerator, unsigned denominator);
@@ -53,24 +55,28 @@ protected:
 
 public:
     /**
-     * @brief Constructs the object
+     * @brief Constructs the object.
      * @param layerAxis axis along which to create layers
      * @param rotationAxis axis along which to perform rotations
      * @param rotationAngle angle by which to perform rotations (either explicit in Radians or FullRotationQuotient)
      * @param isAlternating if @a true, molecules in even layers will be rotated counter-clockwise and clockwise in odd
-     * layers. If @a false, all molecules will be rotated counter-clockwise
+     * layers. If @a false, all molecules will be rotated counter-clockwise. *Note*: for @a isAlternating equal `true`,
+     * the following is implied:
+     * <ul>
+     *   <li> if @a isCumulative equals `true`, even layers are not rotated, while odd layers are rotated clockwise from
+     *        the original position
+     *   <li> if @a isCumulative equals `false`, even layers are rotated counter-clockwise, while odd layers are rotated
+     *        clockwise from the original position
+     * </ul>
      * @param isCumulative if @a true, angle of rotation starts at zero at the lowest layer and is accumulated when
      * moving up the layers, according to @a rotationAngle and @a isAlternating. If @a false, each layer is rotated
-     * independently. *Note*: for @a isAlternating equal `true`, the following is implied:
+     * independently. *Note*: for @a isCumulative equal `true` and @a isAlternating equal `false`
      * <ul>
-     *   <li>
-     *     if @a isCumulative equals `true`, even layers are not rotated, while odd layers are rotated clockwise from
-     *     the original position
-     *   </li>
-     *   <li>
-     *     if @a isCumulative equals `false`, even layers are rotated counter-clockwise, while odd layers are rotated
-     *     clockwise from the original position
-     *   </li>
+     *   <li> if @a rotationAngle is Radians, the whole column of cells along the @a layerAxis will be merged into a
+     *        single unit cell (LayerRotationTransformer::getRequestedNumOfLayers() returns `std::nullopt`)
+     *   <li> if @a rotationAngle is FullRotationQuotient, the minimal number of cells along the @a layerAxis ensuring
+     *        periodicity will be merged into a unit cell (LayerRotationTransformer::getRequestedNumOfLayers() returns
+     *        FullRotationQuotient::denominator)
      * </ul>
      */
     LayerRotationTransformer(LatticeTraits::Axis layerAxis, LatticeTraits::Axis rotationAxis,
